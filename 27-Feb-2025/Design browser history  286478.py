0acd6a67bcf620ffa99f9aaaa4fd74f0bc4de657
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2025-02-27 23:04:44 +0300

    Created 'Design browser history ' by A2SV Hub

---
+++ 27-Feb-2025/Design browser history  286478.py
@@ -0,0 +1,45 @@
+# Problem: Design browser history  - https://leetcode.com/problems/design-browser-history/
+
+class ListNode:
+    def __init__(self, val=0, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+class BrowserHistory:
+    def __init__(self, homepage: str):
+        self.head = ListNode(homepage, None)
+        self.tail = ListNode(0)
+        self.tail.prev = self.head
+        self.curr = self.tail.prev
+
+    def visit(self, url: str) -> None:
+        self.insertAtTail(url)
+
+    def back(self, steps: int) -> str:
+        j = steps
+
+        while j > 0 and self.curr.prev:
+            self.curr = self.curr.prev
+            j -= 1
+        return self.curr.val
+
+    def forward(self, steps: int) -> str:
+        j = steps
+        while j > 0 and self.curr.next:
+            self.curr = self.curr.next
+            j -= 1
+        return self.curr.val
+
+    def insertAtTail(self, item) -> None:
+        v = ListNode(item, None)
+        self.curr.next = v
+        v.prev = self.curr
+        self.curr = self.curr.next
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
