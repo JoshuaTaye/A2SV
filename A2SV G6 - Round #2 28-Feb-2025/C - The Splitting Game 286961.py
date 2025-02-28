11b8bcc41baa54a2d80fc275a50e3ad291eef22a
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2025-02-28 18:19:13 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 28-Feb-2025/C - The Splitting Game 286961.py
@@ -0,0 +1,25 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+n = int(input())
+lst = []
+for j in range(n):
+    x = int(input())
+    y = input()
+    lst.append(y)
+for i in lst:
+    hm = {}
+    for j in i:
+        if j not in hm:
+            hm[j] = 1
+        else:
+            hm[j] += 1
+    sett = set()
+    maxx = 0
+    for k in range(len(i)):
+        sett.add(i[k])
+        if hm[i[k]] > 1:
+            hm[i[k]] -= 1
+        else:
+            del hm[i[k]]
+        maxx = max(maxx, len(hm) + len(sett))
+    print(maxx)
