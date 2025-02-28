ba9c1b659a1b1241938a6f31c4517e706bee5674
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2025-02-28 18:18:18 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 28-Feb-2025/D - Minimize the Distance 286960.py
@@ -0,0 +1,36 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+lst = list(map(int, input().split()))
+ss  = [lst[-1]]
+ps = [lst[0]]
+
+prevd = [0]
+lst.sort()
+minim = [0]
+maxim = [0]
+for i in range(1, len(lst)):
+   minim.append(minim[i-1] + lst[i] - lst[i-1])
+for i in range(1, len(minim)):
+    minim[i] = minim[i-1] + minim[i]
+maxim = list(reversed(minim))
+minn = float("inf")
+minind = -1
+
+for p in range(len(minim)):
+    if minim[p] + maxim[p] < minn:
+        minind = p
+        minn = minim[p] + maxim[p]
+print(lst[minind])
+
+
+# for j in range(len(lst)):
+#     d =
+
+
+# distancea = ps[]
+
+
+# ss = list(reversed(ss))
+# print(ss)
+# print(ps)
