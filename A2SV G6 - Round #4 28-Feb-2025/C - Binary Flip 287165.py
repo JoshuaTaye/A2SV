84a3dc74494098aea80a50c4468d167a963662c2
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2025-02-28 19:55:32 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 28-Feb-2025/C - Binary Flip 287165.py
@@ -0,0 +1,61 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+def flip(n, a, b):
+    summ = 0
+    for j in a:
+        if j == "1":
+            summ += 1
+    r = n - 1
+    flips = 0
+    while r >= 0:
+        if not flips % 2:
+            if a[r] != b[r] and summ * 2 != r+1:
+                return "NO"
+            elif a[r] != b[r]:
+                flips += 1
+                if flips % 2:
+                    while r >= 0 and a[r] != b[r]:
+                        if a[r] == "1":
+                            summ -= 1
+                        r -= 1
+                else:
+                    while r >= 0 and a[r] == b[r]:
+                        if a[r] == "1":
+                            summ -= 1
+                        r -= 1
+            else:
+                if a[r] == "1":
+                    summ -= 1
+                r -= 1
+        else:
+            if a[r] == b[r] and summ * 2 != r+1:
+                return "NO"
+            elif a[r] == b[r]:
+                flips += 1
+                if flips % 2:
+                    while r >= 0 and a[r] != b[r]:
+                        if a[r] == "1":
+                            summ -= 1
+                        r -= 1
+                else:
+                    while r >= 0 and a[r] == b[r]:
+                        if a[r] == "1":
+                            summ -= 1
+                        r -= 1
+            else:
+                if a[r] == "1":
+                    summ -= 1
+                r -= 1
+    return "YES"
+
+
+
+
+total = int(input())
+for i in range(total):
+    n = int(input())
+    a = input()
+    b = input()
+    print(flip(n, a, b))
+
+
