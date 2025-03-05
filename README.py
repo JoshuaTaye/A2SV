abd6ae3c3058d96a012485f0b049c00c6b1929aa
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2025-03-05 11:42:38 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,15 +1,1 @@
-# FizzBuzz
-def fizzbuzz(n):
-    lst=[]
-    for i in range(n+1):
-        if i%3==0 and i%5!=0 and i>0:
-            lst.append('Fizz')
-        elif i%5==0 and i%3!=0 and i>0:
-            lst.append('Buzz')
-        elif i%3==0 and i%5==0 and i>0:
-            lst.append('FizzBuzz')  
-        elif i>0:
-            lst.append(str(i))
-    return(lst)
-n=int(input('Enter No.:'))
-print(fizzbuzz(n))  
+A repository to store my rather embarazzing leetcode submissions. But hey, they worked.
