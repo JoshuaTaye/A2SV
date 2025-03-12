f7cfc2a1a8a9c82bda7b79219349387947aa8e2d
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2025-03-12 08:51:02 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/E - Minimum Subsequence 303509.py
@@ -0,0 +1,22 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+tot = int(input())
+for l in range(tot):
+    n = int(input())
+    x = input()
+    zeros = []
+    ones = []
+    st = []
+    for i in range(len(x)):
+        tot = len(zeros) + len(ones)
+        if x[i] == "0":
+            if ones:
+                tot = ones.pop()
+            zeros.append(tot)
+        else:
+            if zeros:
+                tot = zeros.pop()
+            ones.append(tot)
+        st.append(tot+1)
+    print(len(ones) + len(zeros))
+    print(*st)
