efe35e5e20285af348c6d9ac2a44c5440e126acb
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2025-03-12 08:51:06 +0300

    Created 'F - Nahom's Array Dilemma' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/F - Nahom's Array Dilemma 303510.py
@@ -0,0 +1,29 @@
+# Problem: F - Nahom's Array Dilemma - https://codeforces.com/gym/594077/problem/F
+
+from collections import deque
+
+def s(arr):
+    nn = len(arr)
+    prefix = [0] * (nn + 1)
+    st = []
+    for right, val in enumerate(arr):
+        prefix[right] += prefix[right - 1] + val
+        while st and arr[st[-1]] <= val:
+            left = st.pop()
+            if prefix[right] - prefix[left-1] > val:
+                return False
+        st.append(right)
+    return True
+
+
+tot = int(input())
+for l in range(tot):
+    n = int(input())
+    ar = list(map(int, input().split()))
+    if s(ar) and s(ar[::-1]):
+        print("YES")
+    else:
+        print("NO")
+
+
+
